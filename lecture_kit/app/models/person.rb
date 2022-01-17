92f8b14e088c6f39f8c28b73fd09beb089be25ed
Alessandra Chardon <achardon@ucla.edu>
2022-01-17 12:02:36 -0500

    added behaviors

--- lecture_kit/app/models/person.rb
+++ lecture_kit/app/models/person.rb
@@ -5,4 +5,15 @@ class Person < ActiveRecord::Base
 
     has_many :waterings
     has_many :waterees, through: :waterings
+
+    def water_plant(plant)
+        binding.pry
+        Watering.create(plant_id: plant.id, person_id: self.id)
+        pp = plant_parenthoods.find_by(plant: plant)
+        if pp
+            affection = pp.affection || 1
+            affection += 1
+            pp.update(affection: affection)
+        end
+    end
 end
